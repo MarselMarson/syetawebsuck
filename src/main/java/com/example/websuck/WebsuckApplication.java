209af1fbb12089fa55b8fc4876869ddce7901d47
marsel <marseleene@gmail.com>
2024-09-13 15:28:02 +0300

    init

---
+++ src/main/java/com/example/websuck/WebsuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.websuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsuckApplication.class, args);
+    }
+
+}
